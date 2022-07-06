5a49516b5df9c60fa11ce8bd4922aa0f63872f70
hoonkim <hoonk9387@gmail.com>
2022-07-05 22:07:15 -0500

    Completed assignment

---
+++ app.rb
@@ -0,0 +1,24 @@
+# app.rb
+
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+# puts ("Hello world!")
+
+# ruby -v CHECKS WHICH RUBY VERSION
+# ruby filename.rb RUNS RUBY CODE IN FILE (RELATIVE PATH)
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# puts [1,2,3]
+# puts [1,2,3].to_s
+# p [1,2,3]
+# puts [1,2,3].inspect
+
+# pp [{id: 1, hello: "World"}, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, {id: 4, hello: "Learner"},]
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
